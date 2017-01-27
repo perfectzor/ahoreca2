89e9c469772b736ef98bc1b8a2428996fafa18c3
perfectzor <perfectzor@gmail.com>
2017-01-27 17:25:52 +0000

    dasda

---
+++ app_server/models/clients.js
@@ -0,0 +1,19 @@
+﻿var mongoose = require('mongoose');
+
+var reportsSchema = new mongoose.Schema({
+    vat: { type: Number, required: true }
+    
+});
+
+var clientsSchema = new mongoose.Schema({
+    number: { type: Number, required: true},
+    name: { type: String, required: true },
+    clientvat: { type: Number, required: true },
+    cae: { type: Number, required: false },
+    address: { type: String, required: true },
+    telephone: { type: Number, required: false },
+    email: { type: String, required: true },
+    subscription: { type: String, required: true},
+    reports: [reportsSchema]
+});
+
---
+++ app_server/models/db.js
@@ -0,0 +1,56 @@
+﻿var mongoose = require('mongoose');
+
+var dbURI = 'mongodb://perfectzor:jpr782jpr782@ds117839.mlab.com:17839/heroku_tlwcbx5t';
+mongoose.connect(dbURI);
+
+mongoose.connection.on('connected', function () {
+    console.log('Mongoose connected to ' + dbURI);
+});
+
+mongoose.connection.on('error', function (err) {
+    console.log('Mongoose connection error: ' + err);
+});
+
+mongoose.connection.on('disconnected', function () {
+    console.log('Mongoose disconnected');
+});
+
+var readLine = require("readline");
+if (process.platform === "win32") {
+    var rl = readLine.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    rl.on("SIGINT", function () {
+        process.emit("SIGINT");
+    });
+}
+
+var gracefulShutdown = function (msg, callback) {
+    mongoose.connection.close(function () {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+};
+
+process.once('SIGUSR2', function () {
+    gracefulShutdown('nodemon restart', function () {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+
+process.on('SIGINT', function () {
+    gracefulShutdown('app termination', function () {
+        process.exit(0);
+    });
+});
+
+process.on('SIGTERM', function () {
+    gracefulShutdown('Heroku app shutdown', function () {
+        process.exit(0);
+    });
+});
+
+
+require('./clients');
+require('./users');
