1d9a5e861407f2940e5c3d801c709020e6e46593
João Fernandes <jpacfernandes@gmail.com>
2017-01-31 11:17:22 +0000

    api server reports.js fix

--- app_server/controllers/reports.js
+++ app_server/controllers/reports.js
@@ -3,6 +3,10 @@ var request = require('request');
 var apiOptions = {
     server: "http://localhost:3000"
 };
+if (process.env.NODE_ENV === 'production') {
+    apiOptions.server = "https://ahoreca.herokuapp.com";
+}
+
 
 
 
